ab5a47f009eefe8b4ff9d7a3bf8233b87bb1b2ad
ramos <ramosrunard8@gmail.com>
2026-08-04 14:36:27 +0800

    Fix admin layout duplication and spacing

--- client/src/pages/Dashboard/DashboardAdmin.jsx
+++ client/src/pages/Dashboard/DashboardAdmin.jsx
@@ -1,21 +1,8 @@
-import { useContext } from "react";
-import { UserContext } from "../../../context/userContext";
 import { Outlet } from "react-router-dom";
-import SideNavbar from "../../components/navigation/SideNavbar";
-import RightNavbar from "../../components/navigation/RightNavbar";
 
 export default function DashboardAdmin() {
-	const { user } = useContext(UserContext);
-
 	return (
-		<div className="min-h-screen bg-surface">
-			{/* Side Navigation Bar */}
-			<SideNavbar role="admin" />
-
-			{/* Right Navigation Bar */}
-			<RightNavbar />
-
-			{/* Main content area - Pages handle their own PageLayout */}
+		<div className="min-h-screen bg-gray-50 dark:bg-[#171717]">
 			<Outlet />
 		</div>
 	);
